029c98b9c1cf5375bb52ef6b1367481524a1a77a
Melvin Vasquez <mvasq95@hotmail.com>
2022-11-05 13:59:23 -0400

    mvp for artists card results on desktop

--- src/components/Pages/SearchPage/SearchPage.js
+++ src/components/Pages/SearchPage/SearchPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './SearchPage.scss';
 import Songs from '../../Results/Songs/Songs';
+import Artists from '../../Results/Artists/Artists';
+
 
 const SearchPage = props => {
     return(
         <div className="container" id="search-page">
             <Songs />
+            <Artists />
         </div>        
     )
 }
