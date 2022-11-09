e6bfa563fa127d0735d21e5fe12f121aced9190e
Melvin Vasquez <mvasq95@hotmail.com>
2022-11-08 21:37:28 -0500

    mvp for frontend, proceed to search functionality

--- src/components/Pages/HomePage/HomePage.js
+++ src/components/Pages/HomePage/HomePage.js
@@ -3,8 +3,14 @@ import './HomePage.scss';
 
 const HomePage = props => {
     return(
-        <div className="container">
-            <p>HomePage</p>
+        <div className="container" id="home">
+            <div className="row home-header">
+                <h1>Pseudo Spotify</h1>
+                <p>Search for artists, albums or songs on the search page. Or get a random track from one of my favorite artists on the library page.</p>
+            </div>
+            <div className="row">
+                <img src="https://cdn.mos.cms.futurecdn.net/qB6cCJSi4aqcVZvcSXKbDe.jpg" />
+            </div>
         </div>
     )
 }
