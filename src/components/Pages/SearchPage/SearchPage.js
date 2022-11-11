23d4005c115518d2d416f6ca4cb8dbd8ac05a010
Melvin Vasquez <mvasq95@hotmail.com>
2022-11-10 23:30:18 -0500

    axios post for bearer token working, albeit in unexpected manner

--- src/components/Pages/SearchPage/SearchPage.js
+++ src/components/Pages/SearchPage/SearchPage.js
@@ -9,11 +9,47 @@ import Search from '../../Search/Search';
 
 
 
+
 class SearchPage extends Component {
     constructor(props){
         super(props);
-        this.state ={}
+        this.state ={
+            token:'',
+        }
         //this.function = this.function.bind(this);
+    }
+
+    componentDidMount(){
+        let client_id = process.env.REACT_APP_CLIENT_ID;
+        let client_secret = process.env.REACT_APP_CLIENT_SECRET;
+
+        try{
+
+            const url = 'https://accounts.spotify.com/api/token';
+        
+            const authOptions={
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                auth:{
+                    username: client_id,
+                    password: client_secret
+                }
+            }
+            return axios
+            .post(url, 'grant_type=client_credentials', authOptions)
+            .then(response =>{
+                axios.defaults.headers.common = {
+                    Authorization: `Bearer ${response.data.access_token}`
+                }
+                this.setState({
+                    token: response.data.access_token
+                })
+            })
+    
+        } catch (err){
+            console.log(err)
+        }
     }
 
     render(){
